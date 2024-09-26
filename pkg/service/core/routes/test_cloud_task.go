9c4dd2c10a6769d50cf3992489fac6b4037d1a0c
meng.zhu@nav.no <meng.zhu@nav.no>
2024-09-26 09:55:41 +0200

    use story endpoint

--- pkg/service/core/routes/test_cloud_task.go
+++ pkg/service/core/routes/test_cloud_task.go
@@ -28,7 +28,8 @@ func OrderCreateWorkstationTask() http.HandlerFunc {
 		}
 
 		// Create a Cloud Task to handle the request asynchronously
-		task, err := createHTTPTaskWithAuthCookie(r.Context(), "nada-dev-db2e", "europe-west1", "test-async-arch-markedsplassen", "https://data.ansatt.dev.nav.no/api/workstations", cookie.Value, reqBody)
+		task, err := createHTTPTaskWithAuthCookie(r.Context(), "nada-dev-db2e", "europe-west1", "test-async-arch-markedsplassen",
+			"https://datamarkedsplassen.intern.dev.nav.no/quarto/create", cookie.Value, reqBody)
 		if err != nil {
 			http.Error(w, fmt.Sprintf("Failed to create Cloud Task:%v\n%v\n%v", err, len(cookie.Value), string(reqBody)), http.StatusInternalServerError)
 			return
